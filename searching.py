e62c27fdf9065d492ce57295b8db7925a360235a
Nezvalová Natálie (270858) <270858@vutbr.cz>
2025-04-07 13:07:23 +0200

    pokus1

--- searching.py
+++ searching.py
@@ -1,8 +1,8 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
-
 
 def read_data(file_name, field):
     """
@@ -12,6 +12,15 @@ def read_data(file_name, field):
     :return: (list, string),
     """
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path, "r") as file_obj:
+        data = json.load(file_obj)
+
+    if field in data.keys():
+        return data[field]
+    else:
+        print(f"Field {field} not exist!")
+        return None
+
 
 
 def main():
@@ -20,3 +29,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+    sequential_data = read_data("sequential.json", "unordered numbers")
+    print(sequential_data)
