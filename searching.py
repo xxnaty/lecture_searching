e73d3bb07fa0404a07e34830591e21defb769836
Nezvalová Natálie (270858) <270858@vutbr.cz>
2025-04-07 14:54:15 +0200

    pokus4

--- searching.py
+++ searching.py
@@ -1,6 +1,8 @@
 import os
 import json
 
+from setuptools.dist import sequence
+
 # get current working directory path
 cwd_path = os.getcwd()
 
@@ -30,8 +32,40 @@ def linear_search(sequence, number):
             pass
     return {"positions": list_of_idxs, "count": len(list_of_idxs)}
 
+def pattern_search(sequence, pattern):
+    set_of_idxs = set()
+    pattern_length = len(pattern)
+    for idx in range(0, len(sequence)-pattern_length):
+        pattern_similarity = 0
+        for idx_pattern, pattern_element in enumerate(pattern):
+            if sequence[idx + idx_pattern] == pattern_element:
+                pattern_similarity += 1
+            else:
+                break
+        if pattern_similarity == pattern_length:
+            set_of_idxs.add(idx + pattern_length // 2 - 1)
+        else:
+            pass
+    return set_of_idxs
+
+def binary_search(sequence, number):
+    left = 0
+    right = len(sequence) - 1
+    while right >= left:
+        middle = (left + right) // 2
+        print(sequence[middle])
+        if sequence[middle] == number:
+            return middle
+        elif sequence[middle] > number:
+            right = middle - 1
+        elif sequence[middle] < number:
+            left = middle + 1
+    return None
+
 
 def main():
+    file_name = "sequential.json"
+    key_of_sequence = "dna_sequence"
     sequential_data = read_data("sequential.json", "unordered_numbers")
     print(sequential_data)
     found_numbers = linear_search(sequential_data, 0)
@@ -39,4 +73,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    binary_search(read_data("sequential.json", "ordered_numbers"), 5)
